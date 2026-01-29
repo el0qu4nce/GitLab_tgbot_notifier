f229c20522101ecff2c474510bcab4935f0d2575
el0qu4nce <135351108+el0qu4nce@users.noreply.github.com>
2026-01-29 03:40:01 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+template for config.py file
+"""
+
+TELEGRAM_BOT_TOKEN = "1234567890:JNjcbbBCFIUFCefuhbBCEFUBCJBE&67789HBHD"
+GITLAB_URL = "https://git.aabbccdd.ru"
+USER_CONFIG = {
+
+    1234567890: { # example
+        'gitlab_token': 'glpio-9OpjUO_MH76dW_LKj-QW34rTY6uI8oA4.29.0987GFd43',
+        'project_id': 422
+    },
+}
